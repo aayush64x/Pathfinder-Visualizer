5c0cc79cf846d927a15b164c3f5fa3c605b5476a
aayush64x <aayushnakarmi98@gmail.com>
2026-06-01 18:27:56 -0500

    Implemented BFS

--- src/Algortithm/BFS.ts
+++ src/Algortithm/BFS.ts
@@ -23,3 +23,55 @@ export function getNeighbors(grid: Node[][], node : Node ) : Node[]{
   
   return arr; 
 }
+
+export function bfs(grid: Node[][], startNode : Node, endNode : Node) : [Node[], Node[]]{
+  const visited : Node [] = []
+  let path : Node [] = []
+  const parent = new Map(); 
+  
+  const queue : Node[] = []
+  queue.push(startNode); 
+
+  while (queue.length > 0){
+    let current = queue.shift();
+    if (!current) break
+    let neighbors  = getNeighbors(grid, current);
+
+    if (current.type === "END") {
+      break
+    }
+
+    for(const n of neighbors){
+      const hasNode = visited.some(newNode => newNode.row === n.row && newNode.column === n.column)
+      if(!hasNode){
+        queue.push(n);
+        visited.push(n); 
+        parent.set(n, current);
+      }
+    }
+  }
+
+  path = findPath(parent, startNode, endNode);
+  return [visited, path] 
+}
+
+function findPath( map : Map <Node, Node>, startNode : Node, endNode : Node) : Node[] {
+  const path : Node[] = []
+  let pathFound : boolean = false; 
+  path.push(endNode);
+
+  let current : Node = endNode; 
+  
+  while(!pathFound){
+    let parent = map.get(current);
+    if(!parent) break;
+
+    path.push(parent);
+    if ( parent === startNode){
+      pathFound = true; 
+    }
+    current = parent; 
+    
+  }
+  return path.reverse(); 
+}
